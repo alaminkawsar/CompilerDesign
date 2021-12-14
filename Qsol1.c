15116c7f82bab84578398f6033d474a31d9c2bad
alaminkawsar <alaminkawsar8643@gmail.com>
2021-12-14 17:09:29 +0600

    ActivityA function Done

--- Qsol1.c
+++ Qsol1.c
@@ -2,17 +2,43 @@
 char st[100];
 int n;
 
-void WordCount(){
+
+void ActivityA(){
+
     int wordCount=0, letterCount=0, digitCount=0, otherCount=0;
-    
-    if(st[0]!=' ') wordCount++;
+
     for(int i=0;st[i];i++){
-        if(st[i]!=' ' && st[i+1]==' '){
+        if(st[i]!=' ' && (st[i+1]==' ' || st[i+1]=='\0')){
             wordCount++;
         }
     }
+
+    for(int i=0;st[i];i++){
+        if((st[i]>='a' && st[i]<='z') || (st[i]>='A' && st[i]<='Z')){
+            letterCount++;
+        }
+    }
+
+    for(int i=0;st[i];i++){
+        if(st[i]>='0'&&st[i]<='9'){
+            digitCount++;
+        }
+    }
+
+    int spaceCount=0;
+    for(int i=0;st[i];i++){
+        if(st[i]==' ') spaceCount++;
+    }
+
+    otherCount = strlen(st)-(spaceCount+digitCount+letterCount);
+
+
     printf("Word Count= %d\n",wordCount);
+    printf("Letter Count= %d\n",letterCount);
+    printf("Digit Count= %d\n",digitCount);
+    printf("Other Count= %d\n",otherCount);
 }
+
 
 
 int main()
@@ -20,7 +46,7 @@ int main()
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     scanf("%[^\n]s",st);
-    WordCount();
+    activityA();
     //printf("%s",st);
 
 }
